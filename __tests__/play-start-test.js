c67600c0d17e9d19ad7dcc4b48c506ab77f9fa1f
ywang <657316992@qq.com>
2014-12-29 19:33:52 +0800

    add test to play-start-test.js

--- __tests__/play-start-test.js
+++ __tests__/play-start-test.js
@@ -1,5 +1,4 @@
-jest.dontMock('../js/ordinary-player');
-jest.dontMock('../js/play-start');
+jest.autoMockOff();
 
 describe('PlayStart', function() {
 
@@ -8,23 +7,24 @@ describe('PlayStart', function() {
     it('should return corrcet string', function(){
       console.log = jest.genMockFn();
       var OrdinaryPlayer = require('../js/ordinary-player');
+      var WarriorPlayer = require('../js/warrior-player');
       var PlayStart = require('../js/play-start');
+      var Armor = require('../js/armor');
+      var Weapon = require('../js/weapon');
 
-      var zhangsan = new OrdinaryPlayer('张三',20,3);
-      var li = new OrdinaryPlayer('李斯',20,4);
+      var zhangsan = new OrdinaryPlayer('张三', 50, 10);
+      var armor = new Armor('麒麟甲', 5);
+      var weapon = new Weapon('屠龙刀', 7);
+      var li = new WarriorPlayer('李斯',50, 10, armor, weapon);
 
       var result = PlayStart.start(zhangsan, li);
 
-      var expertString = '张三攻击李斯,李斯受到3点伤害,李斯还剩17血量\n' +
-        '李斯攻击张三,张三受到4点伤害,张三还剩16血量\n' +
-        '张三攻击李斯,李斯受到3点伤害,李斯还剩14血量\n' +
-        '李斯攻击张三,张三受到4点伤害,张三还剩12血量\n' +
-        '张三攻击李斯,李斯受到3点伤害,李斯还剩11血量\n' +
-        '李斯攻击张三,张三受到4点伤害,张三还剩8血量\n' +
-        '张三攻击李斯,李斯受到3点伤害,李斯还剩8血量\n' +
-        '李斯攻击张三,张三受到4点伤害,张三还剩4血量\n' +
-        '张三攻击李斯,李斯受到3点伤害,李斯还剩5血量\n' +
-        '李斯攻击张三,张三受到4点伤害,张三还剩0血量\n' +
+      var expertString = '普通人张三攻击了战士李斯,李斯受到了10点伤害,李斯剩余生命：45\n' +
+        '战士李斯用屠龙刀攻击了普通人张三,张三受到了17点伤害,张三剩余生命：33\n' +
+        '普通人张三攻击了战士李斯,李斯受到了10点伤害,李斯剩余生命：40\n' +
+        '战士李斯用屠龙刀攻击了普通人张三,张三受到了17点伤害,张三剩余生命：16\n' +
+        '普通人张三攻击了战士李斯,李斯受到了10点伤害,李斯剩余生命：35\n' +
+        '战士李斯用屠龙刀攻击了普通人张三,张三受到了17点伤害,张三剩余生命：-1\n' +
         '张三输了';
 
       expect(result).toBe(expertString);
