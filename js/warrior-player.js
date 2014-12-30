089d5c6bf519d8cac506c830d89ea7535a9d9dd3
ywang <657316992@qq.com>
2014-12-30 13:28:24 +0800

    add getCriticalStrikeInformation and getAttackInformation to warrior-plater

--- js/warrior-player.js
+++ js/warrior-player.js
@@ -14,5 +14,31 @@ WarriorPlayer.prototype.attack = function (player) {
   return player.hp - (this.attackForce + weaponAttackForce);
 };
 
+WarriorPlayer.prototype.getAttackInformation = function (player) {
+  var weaponAttackForce = 0;
+  if (this.weapon !== '') {
+    weaponAttackForce = this.weapon.attackForce;
+  }
 
+  var result = '战士' + this.name + '用'+ this.weapon.name +'攻击了普通人' +
+    player.name+ ',' + player.name + '受到了' +
+    (this.weapon.attackForce + this.attackForce) +
+    '点伤害,' + player.name + '剩余生命：' + (player.hp - this.attackForce - weaponAttackForce)  + '\n';
+
+  return result;
+};
+
+WarriorPlayer.prototype.getCriticalStrikeInformation = function (player) {
+  var weaponAttackForce = 0;
+  if (this.weapon !== '') {
+    weaponAttackForce = this.weapon.attackForce;
+  }
+
+  var result = '战士' + this.name + '用'+ this.weapon.name +'攻击了普通人' +
+    player.name+ ',' + this.name + '发动了致命一击,' + player.name + '受到了' +
+    (weaponAttackForce + this.attackForce) * 3 + '点伤害,' +
+    player.name + '剩余生命：' + (player.hp - (this.attackForce + weaponAttackForce) * 3) + '\n';
+
+    return result;
+};
 module.exports = WarriorPlayer;
