49ae226f72fba715647bb23d3d483f2cee17950f
ywang <657316992@qq.com>
2014-12-30 15:25:13 +0800

    update oridinary-player.js

--- js/ordinary-player.js
+++ js/ordinary-player.js
@@ -12,7 +12,13 @@ OrdinaryPlayer.prototype.attack = function (player) {
   if (defensive > this.attackForce) {
     defensive = this.attackForce;
   }
-  return player.hp - (this.attackForce - defensive);
+  player.hp -= (this.attackForce - defensive);
 };
 
+OrdinaryPlayer.prototype.getAttackInformation = function (player) {
+  return '普通人' + this.name + '攻击了战士' +
+    player.name+ ',' + player.name + '受到了' +
+    (this.attackForce - player.armor.defensive) +'点伤害,' +
+    player.name + '剩余生命：' + player.hp + '\n\n';
+};
 module.exports = OrdinaryPlayer;
