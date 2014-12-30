ec19d072f5c669ee00c56c7931ee2b2bea2df376
ywang <657316992@qq.com>
2014-12-30 10:37:14 +0800

    play-start.js start()

--- js/play-start.js
+++ js/play-start.js
@@ -13,8 +13,8 @@ playStart.start = function(zhangsan, li) {
     }
     result += '普通人' + zhangsan.name + '攻击了战士' +
       li.name+ ',' + li.name + '受到了' +
-      zhangsan.attackForce +'点伤害,' +
-      li.name + '剩余生命：' + li.hp + '\n';
+      (zhangsan.attackForce - li.armor.defensive) +'点伤害,' +
+      li.name + '剩余生命：' + li.hp + '\n\n';
 
 
     zhangsan.hp = li.attack(zhangsan);
@@ -26,7 +26,7 @@ playStart.start = function(zhangsan, li) {
     result += '战士' + li.name + '用'+ li.weapon.name +'攻击了普通人' +
       zhangsan.name+ ',' + zhangsan.name + '受到了' +
       (li.weapon.attackForce + li.attackForce) +
-      '点伤害,' + zhangsan.name + '剩余生命：' + zhangsan.hp + '\n';
+      '点伤害,' + zhangsan.name + '剩余生命：' + zhangsan.hp + '\n\n';
   }
   return result;
 }
