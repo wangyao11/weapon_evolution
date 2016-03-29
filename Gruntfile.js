813f65443bcc6e17b17462c95e3f5100b8993833
wangyao11 <657316992@qq.com>
2016-03-29 17:23:54 +0800

    add grunp Gruntfile uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,27 @@
 /**
  * Created by yuxin on 16/3/29.
  */
+module.exports = function (grunt) {
+    // 项目配置
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            //压缩js文件
+            build: {
+                files:[{
+                    expand : true,
+                    cwd    : "js",
+                    src    : "**/*.js",
+                    dest   : "dest/js"
+                }]
+            }
+        }
+    });
+    // 加载提供"uglify"任务的插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // 默认任务
+    grunt.registerTask('default', ['uglify']);
+}
